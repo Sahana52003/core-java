236f47b5385ee5a81c224681a6c97ea791539e57
sahana123xworkz@gmail.com <sahana123xworkz@gmail.com>
2025-11-12 09:31:51 +0530

    init commit

---
+++ enumarations/src/com/xworkz/enumarations/file/States.java
@@ -0,0 +1,36 @@
+package com.xworkz.enumarations.file;
+
+public enum States {
+    ANDHRA_PRADESH("Visakhapatnam", "Wipro"),
+    DELHI("New Delhi", "TCS"),
+    GUJARAT("Gandhinagar", "Infosys"),
+    HARYANA("Gurugram", "Microsoft"),
+    KARNATAKA("Bengaluru", "TCS"),
+    KERALA("Thiruvananthapuram", "Wipro"),
+    MADHYA_PRADESH("Bhopal", "TCS"),
+    MAHARASHTRA("Pune", "Cognizant"),
+    ODISHA("Bhubaneswar",  "Tech Mahindra"),
+    PUNJAB("Mohali", "Infosys"),
+    RAJASTHAN("Jaipur", "Genpact"),
+    TAMIL_NADU("Chennai", "Zoho"),
+    TELANGANA("Hyderabad", "Microsoft"),
+    UTTAR_PRADESH("Noida", "HCL"),
+    WEST_BENGAL("Kolkata", "Wipro");
+
+    public String city;
+    public String companies;
+
+    private States(String city, String companies) {
+        this.city = city;
+        this.companies = companies;
+    }
+
+    public String getCity() {
+
+        return city;
+    }
+
+    public String getCompanies() {
+        return companies;
+    }
+}
---
+++ enumarations/src/com/xworkz/enumarations/runner/Main.java
@@ -0,0 +1,21 @@
+package com.xworkz.enumarations.runner;
+import com.xworkz.enumarations.file.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Country.AFGHANISTAN);
+        System.out.println(Country.AFGHANISTAN.hashCode());
+        System.out.println(Country.ALBANIA.toString());
+        System.out.println(Country.ALBANIA.getCode());
+        System.out.println(Country.ALGERIA.hashCode());
+        System.out.println(Country.ALGERIA.toString());
+        System.out.println(Country.ALGERIA.getCode());
+        System.out.println("--------------------------------------");
+        States states=States.KARNATAKA;
+        System.out.println(States.ANDHRA_PRADESH);
+        System.out.println(States.DELHI.toString());
+        System.out.println(States.ANDHRA_PRADESH.getCompanies());
+        System.out.println(States.ANDHRA_PRADESH.getCity());
+        
+    }
+}
