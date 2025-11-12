53391f5339bc281e048542b9d22b08084e2ad8c5
sahana123xworkz@gmail.com <sahana123xworkz@gmail.com>
2025-11-12 10:17:11 +0530

    init commit

--- enumarations/src/com/xworkz/enumarations/runner/Main.java
+++ enumarations/src/com/xworkz/enumarations/runner/Main.java
@@ -3,13 +3,25 @@ import com.xworkz.enumarations.file.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Country.AFGHANISTAN);
-        System.out.println(Country.AFGHANISTAN.hashCode());
-        System.out.println(Country.ALBANIA.toString());
-        System.out.println(Country.ALBANIA.getCode());
-        System.out.println(Country.ALGERIA.hashCode());
-        System.out.println(Country.ALGERIA.toString());
-        System.out.println(Country.ALGERIA.getCode());
+        System.out.println(Country.AUSTRALIA);
+        System.out.println(Country.AUSTRALIA.hashCode());
+        System.out.println(Country.AUSTRALIA.toString());
+        System.out.println(Country.AUSTRALIA.getCode());
+        System.out.println(Country.BELGIUM.hashCode());
+        System.out.println(Country.BELGIUM.toString());
+        System.out.println(Country.BELGIUM.getCode());
+        System.out.println(Country.CHINA.hashCode());
+        System.out.println(Country.CHINA.toString());
+        System.out.println(Country.CHINA.getCode());
+        System.out.println(Country.INDIA.hashCode());
+        System.out.println(Country.INDIA.toString());
+        System.out.println(Country.INDIA.getCode());
+        System.out.println(Country.JAPAN.hashCode());
+        System.out.println(Country.JAPAN.toString());
+        System.out.println(Country.JAPAN.getCode());
+        System.out.println(Country.SOUTH_KOREA.hashCode());
+        System.out.println(Country.SOUTH_KOREA.toString());
+        System.out.println(Country.SOUTH_KOREA.getCode());
         System.out.println("--------------------------------------");
         States states=States.KARNATAKA;
         System.out.println(States.ANDHRA_PRADESH);
@@ -18,6 +30,17 @@ public class Main {
         System.out.println(States.ANDHRA_PRADESH.getCity());
         System.out.println(States.MADHYA_PRADESH.getCity());
         System.out.println(States.MADHYA_PRADESH.getCompanies());
-        
+        System.out.println(States.GUJARAT.toString());
+        System.out.println(States.GUJARAT.getCity());
+        System.out.println(States.GUJARAT.getCompanies());
+        System.out.println(States.TELANGANA.toString());
+        System.out.println(States.TELANGANA.getCity());
+        System.out.println(States.TELANGANA.getCompanies());
+        System.out.println(States.KERALA.toString());
+        System.out.println(States.KERALA.getCity());
+        System.out.println(States.KERALA.getCompanies());
+        System.out.println(States.TAMIL_NADU.toString());
+        System.out.println(States.TAMIL_NADU.getCompanies());
+        System.out.println(States.TAMIL_NADU.getCity());
     }
 }
