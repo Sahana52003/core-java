66288430f1a168bd44de78d058e941824de51a8e
sahana123xworkz@gmail.com <sahana123xworkz@gmail.com>
2026-02-01 14:43:28 +0530

    Interface(Functional Interface->Predicate)

---
+++ anonymous/predicate/src/com/xworkz/predicate/external/Car.java
@@ -0,0 +1,13 @@
+package com.xworkz.predicate.external;
+
+import com.xworkz.predicate.runner.Vehicle;
+
+import java.util.function.Predicate;
+
+public class Car {
+    public static void main(String[] args) {
+        Predicate<Integer> isEven=num->num%2==0;
+        System.out.println(isEven.test(3));
+        System.out.println(isEven.test(8));
+    }
+}
---
+++ anonymous/predicate/src/com/xworkz/predicate/runner/Vehicle.java
@@ -0,0 +1,6 @@
+package com.xworkz.predicate.runner;
+
+@FunctionalInterface
+public interface Vehicle {
+   boolean test(Integer value);
+}
