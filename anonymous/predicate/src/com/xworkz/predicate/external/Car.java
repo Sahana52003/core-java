16d134d219263a767c68de17754f3c7b2968ab21
sahana123xworkz@gmail.com <sahana123xworkz@gmail.com>
2026-02-05 18:35:57 +0530

    Interface(Functional Interface->Predicate)

--- anonymous/predicate/src/com/xworkz/predicate/external/Car.java
+++ anonymous/predicate/src/com/xworkz/predicate/external/Car.java
@@ -2,7 +2,9 @@ package com.xworkz.predicate.external;
 
 import com.xworkz.predicate.runner.Vehicle;
 
+import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Car {
     public static void main(String[] args) {
